fa247ad95c4396588f5f8836472fbd57ab133b6b
gitPrintln <ghdcks101@naver.com>
2023-03-20 15:52:03 +0900

    0320 당근나라 베이스!!

---
+++ src/main/java/com/carrot/nara/CarrotNaraApplication.java
@@ -0,0 +1,13 @@
+package com.carrot.nara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotNaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotNaraApplication.class, args);
+	}
+
+}
