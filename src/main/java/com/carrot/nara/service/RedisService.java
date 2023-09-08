0599bacb4df7fa59cdccdbd779c553cfa16e1108
gitPrintln <ghdcks101@naver.com>
2023-09-08 16:20:37 +0900

    `

--- src/main/java/com/carrot/nara/service/RedisService.java
+++ src/main/java/com/carrot/nara/service/RedisService.java
@@ -21,24 +21,24 @@ public class RedisService {
     }
     
     @Autowired
-    private MessageRespository messageRespository; // 캐시가 없으면 마지막 챗을 가져옴
+    private MessageRespository messageRespository; // lastChat 캐시가 없으면 마지막 챗을 가져옴
 
-    // redis에 캐시를 설정해둠.
+    // redis에 lastChat 캐시를 설정해둠.
     private void setCacheLastChat(Integer chatId, String lastChat) {
         log.info("cacheLastChat()");
-        String id = "chatId: " + chatId;
+        String id = "lastChat. chatId: " + chatId;
         redisTemplate.opsForValue().set(id, lastChat);
     }
 
-    // redis cache의 lastchat을 가져옴.
+    // redis cache 중 lastChat을 가져옴.
     private String getLastChatFromCache(Integer chatId) {
         log.info("getLastChatFromCache()");
-        String id = "chatId: " + chatId;
+        String id = "lastChat. chatId: " + chatId;
         return redisTemplate.opsForValue().get(id);
     }
     
     /**
-     * 1. redis 캐시 데이터를 확인 후 없으면 2. jpaRepository에서 찾아옴 + 캐시에 저장.
+     * 1. redis lastChat 캐시 데이터를 확인 후 없으면 2. jpaRepository에서 찾아옴 + lastChat 캐시에 저장.
      * @param chatId 마지막 챗을 가져올 chatId
      * @return 마지막 챗
      */
@@ -53,10 +53,36 @@ public class RedisService {
         return lastChat;
     }
 
-    // lastChat이 바뀌었을 경우. redis 캐시도 수정해줌.
+    // lastChat이 바뀌었을 경우. redis lastChat 캐시도 수정해줌.
     public void modifiedLastChat(Integer chatId, String message) {
         log.info("modifiedLastChat()");
-        String id = "chatId: " + chatId;
+        String id = "lastChat. chatId: " + chatId;
         redisTemplate.opsForValue().set(id, message);
+    }
+    
+    // 채팅방에 로그인 되어 있는 지 조회(있으면 true, 없으면 false)
+    public boolean isLogInChatRoom(Integer chatId, Integer userId) {
+        log.info("loginChatRoom()");
+        String ids = "loginuser. chatId: " + chatId + ", userId: " + userId;
+        return redisTemplate.hasKey(ids);
+    }
+    
+    // 채팅방에 로그인 중인 유저로 등록. loginuser key값 등록
+    public void registerLogInChatRoom(Integer chatId, Integer userId) {
+        log.info("registerLoginChatRoom()");
+        String ids = "loginuser. chatId: " + chatId + ", userId: " + userId;
+        redisTemplate.opsForValue().set(ids, "Log In");
+    }
+    
+    // 채팅방에서 로그아웃 했으므로 loginuser key값 삭제
+    public void logOutChatRoom(Integer chatId, Integer userId) {
+        log.info("logOutChatRoom()");
+        String ids = "loginuser. chatId: " + chatId + ", userId: " + userId;
+        redisTemplate.delete(ids);
+    }
+    
+    // 메세지를 안읽음
+    public void readMessage() {
+        
     }
 }
